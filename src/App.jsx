4f07ab35ba987c2261cd2f71d215b7db07510ce8
Hom Dahal <homdahal997@gmail.com>
2024-06-11 17:19:19 -0400

    refactor: Update API key variable in App component

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,29 @@ import Form from "../components/Form";
 
 export default function App() {
   // Variable with your API Key
-  const apiKey = "c7873040";
+  const apiKey = "98e3fb1f";
 
   // State to hold movie data
   const [movie, setMovie] = useState(null);
 
   // Function to get movies
-  const getMovie = async (searchTerm) => {
-    // Make fetch request and store the response
-    const response = await fetch(
-      `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
-    );
-    // Parse JSON response into a JavaScript object
-    const data = await response.json();
-    // Set the Movie state to the received data
-    setMovie(data);
-  };
+  const getMovie = async(searchTerm) => {
+    try {
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+      );
+      const data = await response.json();
+      setMovie(data);
+    } catch(e) {
+      console.error(e)
+    }
+  }
+
+  
+  // This will run on the first render but not on subsquent renders
+  useEffect(() => {
+    getMovie("Clueless");
+  }, []);
 
   // We pass the getMovie function as a prop called moviesearch
   // We pass movie as props to movie display
