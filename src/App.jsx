3968903f2e9170f77fde6b8645f4ebe952f97de2
Hom Dahal <homdahal997@gmail.com>
2024-06-11 17:16:08 -0400

    form and movie display component added

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,30 @@ import MovieDisplay from "../components/MovieDisplay";
 import Form from "../components/Form";
 
 export default function App() {
+  // Variable with your API Key
+  const apiKey = "c7873040";
+
+  // State to hold movie data
+  const [movie, setMovie] = useState(null);
+
+  // Function to get movies
+  const getMovie = async (searchTerm) => {
+    // Make fetch request and store the response
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+    );
+    // Parse JSON response into a JavaScript object
+    const data = await response.json();
+    // Set the Movie state to the received data
+    setMovie(data);
+  };
+
+  // We pass the getMovie function as a prop called moviesearch
+  // We pass movie as props to movie display
   return (
     <div className="App">
-      <Form />
-      <MovieDisplay />
+      <Form moviesearch={getMovie} />
+      <MovieDisplay movie={movie} />
     </div>
   );
 }
